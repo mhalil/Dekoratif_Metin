97ac07cf76ae749833d726890fd2155e381e571e
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-06-11 23:37:03 +0300

    Küçük harf yazınca ile çalışıyor
    Girdi metnini küçük harflerle yazınca kod doğru çalışıyor.
    Büyük harf kullanınca sorun çıkıyor.
    Şuan tanılı harfler : a,h,i ve l'dir.

--- dijitale_cevir.py
+++ dijitale_cevir.py
@@ -1,43 +1,26 @@
-#h = """
-##   #
-##   # 
-######
-##   #
-##   #"""
-#
-#h0="#   #"
-#h1="#   #"
-#h2="#####"
-#h3="#   #"
-#h4="#   #"
-#
-#a0="#####"
-#a1="#   #"
-#a2="#####"
-#a3="#   #"
-#a4="#   #"
-#
-#l0="#    "
-#l1="#    "
-#l2="#    "
-#l3="#    "
-#l4="#####"
-#
-#i0="  #  "
-#i1="     "
-#i2="  #  "
-#i3="  #  "
-#i4="  #  "
+alf= {"a0" : "#####", "a1" : "#   #", "a2" : "#####", "a3" : "#   #", "a4" : "#   #", "h0" : "#   #", "h1" : "#   #",  "h2" : "#####", "h3" : "#   #", "h4" : "#   #", "i0" : "  #  ", "i1" : "     ", "i2" : "  #  ", "i3" : "  #  ", "i4" : "  #  ", "l0" : "#    ", "l1" :"#    ", "l2" : "#    ", "l3" : "#    ", "l4" : "#####"}
 
-#h=["#   #", "#   #", "#####", "#   #", "#   #"]
-
-
-alf= {"a0" : "#####", "a1" : "#   #", "a2" : "#####", "a3" : "#   #", "a4" : "#   #", "h0" : "#   #", "h1" : "#   #",  "h2" : "#####", "h3" : "#   #", "h4" : "#   #"}
-
-metin = [] # yazılan metine karşılık gelen harf kodları, yazdırmaya hazırlamak için sırayla bu listeye eklenecek.
+mkod = [] # yazılan metine karşılık gelen harf kodları, yazdırmaya hazırlamak için sırayla bu listeye eklenecek.
+sayac = 0
+sonuc = ""
 
 girdi = input("metni yazın: ")  #halil
+girdi = girdi.lower()
 
-for harf in girdi:
-    print(alf[harf + "0"]) # "0", "1", ...vb deerleri yerine indeks değeri eklenmeli
+for _ in range(5):
+    for harf in girdi:
+        mkod.append(alf[harf + str(sayac)])
+        mkod.append(" ")
+    mkod.append("\n")
+    sayac += 1
+    
+print(mkod)
+
+    
+for i in mkod:
+    sonuc += i
+
+# sonuc = sonuc.replace("#", "-") # "#" işareti yerine başka bir karakter kullanmak istersek burada ayarlayarak kullanabiliriz.
+                      
+print(sonuc)
 
