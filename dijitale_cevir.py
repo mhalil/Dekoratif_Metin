52f828d3c68bf016449355413d71e45f81da98be
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-06-12 12:06:44 +0300

    karakter kodlarındaki fazla boşluklar silindi.
    ı ve i (harflerinin) keylerinin value karşılığındaki  "  #  " gibi ibareler "#" şekline çevrildi. Karakterler minimum genişliğe ayarlandı.
    Büyük ve küçük harf yazınca da kod çalışıyor ancak, büyük I harfini, küçük harfe çevirirken i olarak

--- dijitale_cevir.py
+++ dijitale_cevir.py
@@ -1,4 +1,4 @@
-alf= {"a0" : "#####", "a1" : "#   #", "a2" : "#####", "a3" : "#   #", "a4" : "#   #", "h0" : "#   #", "h1" : "#   #",  "h2" : "#####", "h3" : "#   #", "h4" : "#   #", "i0" : "  #  ", "i1" : "     ", "i2" : "  #  ", "i3" : "  #  ", "i4" : "  #  ", "l0" : "#    ", "l1" :"#    ", "l2" : "#    ", "l3" : "#    ", "l4" : "#####"}
+alf= {"a0" : "#####", "a1" : "#   #", "a2" : "#####", "a3" : "#   #", "a4" : "#   #", "h0" : "#   #", "h1" : "#   #",  "h2" : "#####", "h3" : "#   #", "h4" : "#   #", "ı0" : "#", "ı1" : "#", "ı2" : "#", "ı3" : "#", "ı4" : "#", "i0" : "#", "i1" : " ", "i2" : "#", "i3" : "#", "i4" : "#", "l0" : "#    ", "l1" :"#    ", "l2" : "#    ", "l3" : "#    ", "l4" : "#####"}
 
 mkod = [] # yazılan metine karşılık gelen harf kodları, yazdırmaya hazırlamak için sırayla bu listeye eklenecek.
 sayac = 0
@@ -7,20 +7,17 @@ sonuc = ""
 girdi = input("metni yazın: ")  #halil
 girdi = girdi.lower()
 
-for _ in range(5):
+for _ in range(5):  # büyük har fkullanıca bu satırda hata veriyor.
     for harf in girdi:
         mkod.append(alf[harf + str(sayac)])
-        mkod.append(" ")
+        mkod.append(" ") # karakterler arasına eklenecek boşluk ya da özel karakter
     mkod.append("\n")
     sayac += 1
-    
-print(mkod)
-
     
 for i in mkod:
     sonuc += i
 
-# sonuc = sonuc.replace("#", "-") # "#" işareti yerine başka bir karakter kullanmak istersek burada ayarlayarak kullanabiliriz.
+# sonuc = sonuc.replace("#", "*") # "#" karakterini değiştirmek isterseniz, yerine kullaınmasını istediğiniz karakteri bura belirtin.
                       
 print(sonuc)
 
