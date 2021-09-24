de693ce0007b625b46b2898e9face3b16cd7add1
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-09-24 23:39:43 +0300

    Tüm Karakterler Doğru Görüntülendi.
    fonts klasöründeki yazitiplerinin tamamı için kodlar doğru sonuç verdi.

--- Dijital metin v2.0 (FIGLET).py
+++ Dijital metin v2.0 (FIGLET).py
@@ -1,5 +1,4 @@
-# kodların amacı, belirtilen FLF dosyasındaki fazlalıkları silip sade, kullanılabilir sözlük yapısı oluşturmaktır.
-
+# Yazıtipi İsimlerini Gösteren Bir Seçim Menüsü oluşturuldu.
 print("""
 Kullanılabilir Yazıtipleri;
 **************************
@@ -14,86 +13,76 @@ Kullanılabilir Yazıtipleri;
 9. Univers
 """)
 
-secim = input("Listedeki yazı tiplerinden birini seçin (Sadece sayı değeri girin) : ") # "input" ile kullanıcıdan seçim yapması istenecek.
-yazitipi_sec = {"1" : "banner3", "2" : "colossal" , "3" : "doh" ,"4" : "epic" , "5" : "isometric1", "6" : "isometric2", "7" : "isometric3" , "8" : "isometric4" , "9" : "univers"} # seçime karşılık gelen yazıtipini sözlük ile belirle.
-yazitipi = yazitipi_sec[secim]
-yazitipi_yolu = "fonts/" + yazitipi + ".flf"
+secim = input("Listedeki yazı tiplerinden birini seçin (Sadece sayı değeri girin) : ") # Kullanıcıdan Menü seçeneklerinden birini seçmesi isteniyor.
+yazitipi_sec = {"1" : "banner3", "2" : "colossal" , "3" : "doh" ,"4" : "epic" , "5" : "isometric1", "6" : "isometric2", "7" : "isometric3" , "8" : "isometric4" , "9" : "univers"}      # Seçime karşılık gelen yazıtipini sözlük ile belirle.
+yazitipi = yazitipi_sec[secim]                      # Seçilen yazıtipinin adı.
+yazitipi_yolu = "fonts/" + yazitipi + ".flf"        # Yazıtipinin tam yolu.
 
-def ky():       # Yazıtipini oluşturan karakterlerin yuksekligi (her karakterin kaç satırdan oluştuğunu) hesaplayan fonksiyon.
-    yukseklik = 0          # Karakter Yüksekliği (satır sayısı) 
+def ky():                                           # Yazıtipini oluşturan karakterlerin yuksekligi (her karakterin kaç satırdan oluştuğunu) hesaplayan fonksiyon.
+    yukseklik = 0                                   # Karakter Yüksekliği (satır sayısı) 
           
     with open (yazitipi_yolu , "r", encoding="utf-8") as dosya:
-        liste = dosya.readlines()
-        # print(liste)
-        for oge in liste:      # harf yüksekliği hesaplanıyor.
-            if (oge[-3:] == "@@\n"):
-                break       # harf yüksekliğinin hesaplanma işlemi bu satır ile sonlanıyor.
+        liste = dosya.readlines()                   # Yazıtipi dosyasını oku, Listele.
+        
+        for oge in liste:                           # harf yüksekliği hesaplanıyor.
+            if (oge[-3:] == "@@\n"):                # ekseriyetle satır sonları @ ile, karakter sonları (sınırları) @@ ile bitiriliyor. Zaman zaman @ yerine # kullanıldığı da oluyor.
+                break                               # harf yüksekliğinin sınırı ile karşılaşınca işlemi durdur.
 
-            elif ((oge[-2:] == "@\n") or (oge[-3:] == "$@\n") or (oge == "\x7f\x7f\x7f\x7f\x7f@\n")):
-                yukseklik += 1  
+            elif ((oge[-2:] == "@\n") or (oge[-3:] == "$@\n") or (oge == "\x7f\x7f\x7f\x7f\x7f@\n")):       # satırın tamamına, son 2 ve 3 karakterilerine bakarak karakter yüksekliği sonlanmış mı? kontrol et.
+                yukseklik += 1                      # karakter yüksekliği sonlanmamışsa (sınırlanmamışsa) yükseklik değerini bir artır.
                  
-    # print(f"{yazitipi} yazıtipinin karakter yüksekliği {yukseklik+1} satırdır.")
-    return (yukseklik+1)
+    return (yukseklik+1)                            # Yazıtipinin karakter yüksekliğini fonksiyona değer olarak ata.
 
-def listele():      # belirtilen yazitipi dosyasındaki "@\n" ve "@@\n" karakterlerini silerek liste oluştur.
+def listele():                                      # Belirtilen yazitipi dosyasındaki "@\n" ve "@@\n" karakterlerini silerek liste oluştur.
     alfabe_kodlari = list()
 
     with open (yazitipi_yolu , "r", encoding="utf-8") as dosya:
         liste = dosya.readlines()
         for i in liste:
             if (i[-2:] != "@\n"):
-                pass
+                if ( (i[-2:] == "#\n") and (i[-3:] != "##\n")):     # sonu @ ile bitmeyen karakterlerde ekseriyetle @ yerine # kullanılıyor. "doh" yazitipinde @ işareti, aynı karakterden oluşturulduğu için bitiş parametresi # ile belirtilmiş.
+                    alfabe_kodlari.append(i[:-2])
+
+                elif (i[-3:] == "##\n"):
+                    alfabe_kodlari.append(i[:-3])
+
             else:
-                if ( (i[-2:] == "@\n") and (i[-3:] != "@@\n")):
+                if ( (i[-2:] == "@\n") and (i[-3:] != "@@\n")):     
                     alfabe_kodlari.append(i[:-2])
                 
                 elif (i[-3:] == "@@\n"):
                     alfabe_kodlari.append(i[:-3])
                   
-    # print(alfabe_kodlari)
-    return alfabe_kodlari
+    return alfabe_kodlari                           # Alfabeyi oluşturan kodlara ait listeyi, fonksiyona değer olarak ata.
 
 def alfabe_sozluk():
-    karakterler = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÂÖÜâöüβ""" # Yazıtipi dosyalarında bulunan sıralı (en fazla) karakter bilgisi.
+    karakterler = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ÂÖÜâöüβ""" # Yazıtipi dosyalarında bulunan sıralı karakter bilgisi.
     
-    krt_yuk = ky()
-    alfabe_kod = listele()
-    alfabe_key = list()
-    alfabe = dict()
+    krt_yuk = ky()                                  # Karakter YÜksekliği değerini ky() fonksiyonun çalıştırarak hesapla ve krt_yuk değişkenine ata.
+    alfabe_kod = listele()                          # Alfabeye ait kodları listele() fonksiyonun çalıştırarak hesapla ve alfabe_kod değişkenine ata.
+    alfabe_key = list()                             # alfabe_key isimi boş bir liste oluştur.
+    alfabe = dict()                                 # alfabe isimi boş bir sözlük yapısı oluştur.
     
-    for k in karakterler:       # karakterlere sayı numarası eklenip alfabe adlı sözlüğe kaydediliyor.
-        for l in range(1,krt_yuk+1):
-            alfabe_key.append(k+str(l))
+    for k in karakterler:                           # Karakterlere sayı numarası eklenip alfabe_key listesine kaydeden döngü yapısı.
+        for l in range(1,krt_yuk+1):                # Sayılar 1'den başlayarak karakter (satır) yüksekliğine kadar devam edecek.
+            alfabe_key.append(k+str(l))             # listeye ekle. Ör: A1, A2, A3, ..., z1, z2, z3...vb
 
-    for i in range(len(alfabe_kod)):
+    for i in range(len(alfabe_kod)):                # alfabe_kod listesindeki öğeleri, alfabe_key listesindek öğelerle sırasına uygun olarak eşleştirip alfabe isimli sözlüğe ekleyen döngü yapısı. Ör: {'a1' : '## # #' }
         alfabe[alfabe_key[i]] = alfabe_kod[i]
 
-    # print(alfabe_kod)
-    # print(alfabe_key)
-    return alfabe
+    return alfabe                                   # Oluşturulan alfabe isimli sözlüğü fonksiyona değer olarak ata.
 
-def sonuc():
-    sozluk = alfabe_sozluk()
-    yuk = ky()
-    # girdi = input("Metni yazın: ")
-    girdi = "Mustafa"
-    sonuc = ""
+def sonuc():                                        # Kullanıcıdan Yazitipi menüsünden seçim yapmasını ve bir metin yazmasını isteyen; Yazılan metni, seçilen yazıtipi kodlarına çevirerek ekrana yazdıran fonksiyon.
+    sozluk = alfabe_sozluk()                        # alfabe_sozluk() fonksiyonunu çalıştır ve sozluk isimli değişkene ata. 
+    yuk = ky()                                      # ky() fonksiyonunu çalıştırarak, karakter (satır) yüksekliğini hesaplaya ve yuk isimli değişkene ata.
+    girdi = input("Metni yazın: ")                  # Kullanıcıdan bir metin girmesini iste.
+    sonuc = ""                                      # sonuc isimli boş bir string oluştur.
 
-    for i in range(1, yuk+1): 
+    for i in range(1, yuk+1):                       # Kullanıcının yazdığı metni sozluk yapısından sorgulayarak her karaktere karşılık gelen kodları sonuc isimli stringe ekleyen döngü yapısı.
         for harf in girdi:
             sonuc += sozluk[harf + str(i)] + "   "
         sonuc += "\n"
 
-    print(sonuc.replace("$", " "))
+    print(sonuc.replace("$", " "))                  # sonuc isimli stringdeki (karakter sonlarındaki) $ karakterini silerek sonucu ekrna yazdır.
 
-# print(ky())
-# print(listele())
-# print(alfabe_sozluk())
-
-# ky()
-# listele()
-# alfabe_sozluk()
-sonuc()
-
-
-
+sonuc()                                             # sonuc() fonksiyonunu çalıştır.
