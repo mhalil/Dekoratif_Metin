583ed984eb49f5e9ec424ef50fb76daa0ac5ed5d
Mustafa Halil <halil.mustafa@gmail.com>
2022-05-19 20:48:44 +0300

    Pencere boyutu artirildi

--- Dekoratif_Metin_[FIGLET]_v3_GUI.py
+++ Dekoratif_Metin_[FIGLET]_v3_GUI.py
@@ -69,46 +69,44 @@ def alfabe_sozluk():
     return alfabe                                   # Oluşturulan alfabe isimli sözlüğü fonksiyona değer olarak ata.
 
 ## BUTON Fonksiyonları
-# Kullanıcı metin yazıp, Listeden Yazitipi seçtikten sonra, Yazılan metni, seçilen yazıtipi kodlarına çevirerek cikti ekranına yazdıran fonksiyon.
-def donustur_yatay():                                        
+def donustur_yatay():           # Kullanıcı metin yazıp, Listeden Yazitipi seçtikten sonra, Yazılan metni, seçilen yazıtipi kodlarına çevirerek cikti ekranına YATAY olarak yazdıran fonksiyon.
 
-    yuk = ky()                                  # ky() fonksiyonunu çalıştırarak, karakter (satır) yüksekliğini hesaplaya ve yuk isimli değişkene ata.
-    sozluk = alfabe_sozluk()                    # alfabe_sozluk() fonksiyonunu çalıştır ve sozluk isimli değişkene ata.                                       
-    girdi_metni = girdi.get()                   # kullanıcının girdiği metni al.
-    sonuc = ""                                  # sonuc isimli boş bir string oluştur.
+    yuk = ky()                  # ky() fonksiyonunu çalıştırarak, karakter (satır) yüksekliğini hesaplaya ve yuk isimli değişkene ata.
+    sozluk = alfabe_sozluk()    # alfabe_sozluk() fonksiyonunu çalıştır ve sozluk isimli değişkene ata.                                       
+    girdi_metni = girdi.get()   # kullanıcının girdiği metni al.
+    sonuc = ""                  # sonuc isimli boş bir string oluştur.
     
-    y_tipi_adi = yazitipi.get()                 # Açılır listede seçili olan yazıtipi adı
+    y_tipi_adi = yazitipi.get() # Açılır listede seçili olan yazıtipi adı
 
-    if y_tipi_adi != "Standard":                     # "standard" isimli yazıtiplerinde daha az sayıda karakter var, eğer o yazıtipleri seçilirse Türkçe karakterler yerine diğer karakterleri kullan.
+    if y_tipi_adi != "Standard":    # "standard" isimli yazıtiplerinde daha az sayıda karakter var, eğer o yazıtipleri seçilirse Türkçe karakterler yerine diğer karakterleri kullan.
         turkce_karakterler = {"ç" : "c", "Ç" : "C", "ğ" : "g", "Ğ" : "G", "ı" : "i", "İ" : "I", "ş" : "s", "Ş" : "S",  "ö" : "o", "Ö" : "O", "ü" : "u", "Ü" : "U"}       # hangi harfe yerine hangi hrf kullanılacak, onu belirle.
 
-        for i in range(1, yuk+1):               # Kullanıcının yazdığı metni sozluk yapısından sorgulayarak her karaktere karşılık gelen kodları sonuc isimli stringe ekleyen döngü yapısı.
+        for i in range(1, yuk+1):   # Kullanıcının yazdığı metni sozluk yapısından sorgulayarak her karaktere karşılık gelen kodları sonuc isimli stringe ekleyen döngü yapısı.
             for harf in girdi_metni:
                 if harf in turkce_karakterler.keys():   # Girdi içerisinde Türkçe karakter varsa, kod hata vermesin alfabede ona yakın karakteri kullansın. Ör: ş yerine s, Ğ yerine G, ...vb
                     harf = turkce_karakterler[harf]
                 sonuc += sozluk[harf + str(i)] + "   "
             sonuc += "\n"
     
-    else:                                       # "standart" isimli yazıtipi içerisinden 331 karakter var. Bu durumda, girdide yazılan kelimeleri olduğu gibi (karakterleri değiştirmeden) kullan.
-        for i in range(1, yuk+1):               # Kullanıcının yazdığı metni sozluk yapısından sorgulayarak her karaktere karşılık gelen kodları sonuc isimli stringe ekleyen döngü yapısı.
+    else:                           # "standart" isimli yazıtipi içerisinden 331 karakter var. Bu durumda, girdide yazılan kelimeleri olduğu gibi (karakterleri değiştirmeden) kullan.
+        for i in range(1, yuk+1):   # Kullanıcının yazdığı metni sozluk yapısından sorgulayarak her karaktere karşılık gelen kodları sonuc isimli stringe ekleyen döngü yapısı.
             for harf in girdi_metni:
                 sonuc += sozluk[harf + str(i)] + "   "
             sonuc += "\n"
         
-    dekoratif_metin = sonuc.replace("$", " ")        # sonuc isimli stringdeki (karakter sonlarındaki) $ karakterini silerek sonucu ekrna yazdır.
-    cikti.insert(tk.END, y_tipi_adi + "\n")
+    dekoratif_metin = sonuc.replace("$", " ")   # sonuc isimli stringdeki (karakter sonlarındaki) $ karakterini silerek sonucu ekrna yazdır.
     cikti.insert(tk.END, dekoratif_metin + "\n")
 
-def donustur_dikey():
+def donustur_dikey():           # Kullanıcı metin yazıp, Listeden Yazitipi seçtikten sonra, Yazılan metni, seçilen yazıtipi kodlarına çevirerek cikti ekranına DİKEY olarak yazdıran fonksiyon.
    
-    yuk = ky()                                  # ky() fonksiyonunu çalıştırarak, karakter (satır) yüksekliğini hesaplaya ve yuk isimli değişkene ata.
-    sozluk = alfabe_sozluk()                    # alfabe_sozluk() fonksiyonunu çalıştır ve sozluk isimli değişkene ata.                                       
-    girdi_metni = girdi.get()                   # kullanıcının girdiği metni al.
-    y_tipi_adi = yazitipi.get()                 # Açılır listede seçili olan yazıtipi adı
+    yuk = ky()                  # ky() fonksiyonunu çalıştırarak, karakter (satır) yüksekliğini hesaplaya ve yuk isimli değişkene ata.
+    sozluk = alfabe_sozluk()    # alfabe_sozluk() fonksiyonunu çalıştır ve sozluk isimli değişkene ata.                                       
+    girdi_metni = girdi.get()   # kullanıcının girdiği metni al.
+    y_tipi_adi = yazitipi.get() # Açılır listede seçili olan yazıtipi adı
 
     yeni_girdi = ""
 
-    if y_tipi_adi != "Standard":                     # "standard" isimli yazıtiplerinde daha az sayıda karakter var, eğer o yazıtipleri seçilirse Türkçe karakterler yerine diğer karakterleri kullan.
+    if y_tipi_adi != "Standard":    # "standard" isimli yazıtiplerinde daha az sayıda karakter var, eğer o yazıtipleri seçilirse Türkçe karakterler yerine diğer karakterleri kullan.
         turkce_karakterler = {"ç" : "c", "Ç" : "C", "ğ" : "g", "Ğ" : "G", "ı" : "i", "İ" : "I", "ş" : "s", "Ş" : "S", "ö" : "o", "Ö" : "O", "ü" : "u", "Ü" : "U"}       # hangi harfe yerine hangi hrf kullanılacak, onu belirle.
 
         for harf in girdi_metni:    # Kullanıcının yazdığı metinde Türkçe karakter varsa, bu karakterleri değiştir ve yeni_girdi degiskenine ekle. Ör: ç => c, ş => s
@@ -126,7 +124,7 @@ def donustur_dikey():
             cikti.insert(tk.END, "\n")
             
     
-    else:                                       # "standart" isimli yazıtipi içerisinden 331 karakter var. Bu durumda, girdide yazılan kelimeleri olduğu gibi (karakterleri değiştirmeden) kullan.
+    else:                           # "standart" isimli yazıtipi içerisinden 331 karakter var. Bu durumda, girdide yazılan kelimeleri olduğu gibi (karakterleri değiştirmeden) kullan.
         for harf in girdi_metni:
             for sayi in range(1, yuk+1):
                 sonuc = sozluk[harf + str(sayi)].replace("$", " ")
@@ -135,11 +133,11 @@ def donustur_dikey():
             cikti.insert(tk.END, "\n")
 
 
-def temizle():
+def temizle():  # girdi ve çıktı ekranlarını temizleyen fonksiyon.
    girdi.delete(0,tk.END)
    cikti.delete("1.0",tk.END)
 
-def kaydet():
+def kaydet():   # çıktı ekranındaki dekoratif metni "Dekoratif_Metin.txt" ismi ile aynı dizine kaydeden fonksiyon.
     metin = girdi.get()
     dekoratif_metin = int(cikti.index("end")[:-2]) - 2
     if dekoratif_metin > 0:
@@ -149,25 +147,25 @@ def kaydet():
         messagebox.showinfo("Kaydedilecek Veri Yok!","Dekoratif Metin Üretilmediği için, kayıt işlemi gerçekleştirilemiyor.")
 
 
-def kopyala():    # Cikti penceresindeki Metni Panoya (clipboard'da) kopyalar
-    pencere.clipboard_clear()  # Optional.
+def kopyala():  # Cikti penceresindeki Metni Panoya (clipboard'da) kopyalar
+    pencere.clipboard_clear()
     pencere.clipboard_append(cikti.get('1.0', tk.END).rstrip())
 
 # ARABİRİM
 pencere = tk.Tk()
-pencere.geometry("605x450+500+300")
+pencere.geometry("750x650+500+200")
 pencere.resizable(0, 0)
 pencere.title(".:: Dekoratif Metin [ FIGLET ] ::.")
 
 etiket_girdi = tk.Label(pencere, text = "Dönüştürülecek Metni Yazın:")
 etiket_girdi.place(x=10, y=10)
 
-girdi = tk.Entry(pencere, width=35)
+girdi = tk.Entry(pencere, width=45)
 girdi.focus()
 girdi.place(x=10, y=40)
 
 aciklama = tk.Label(text = "Kullanmak istediğiniz Yazıtipini seçin.")
-aciklama.place(x=325, y=10)
+aciklama.place(x=425, y=10)
 
 # Yazıtipi Listesi
 secili_yazitipi = tk.StringVar()
@@ -175,62 +173,62 @@ yazitipi = ttk.Combobox(pencere, textvariable=secili_yazitipi, width=30)
 yazitipi["values"] = ("Banner3", "Colossal", "Doh", "Epic", "Isometric1", "Isometric2", "Isometric3", "Isometric4", "Standard", "Univers")
 yazitipi['state'] = 'readonly'
 yazitipi.set("Banner3")
-yazitipi.place(x=325, y=40)
+yazitipi.place(x=425, y=40)
 
 # Sonucun yazdırılacağı Metin ekranı
-cikti = tk.Text(pencere, width=70, height=17, wrap=NONE)
+cikti = tk.Text(pencere, width=87, height=28, wrap=NONE)
 cikti.place(x=30, y=80)
 
 dikey_kaydirma = Scrollbar(pencere)
-dikey_kaydirma.place(x=10, y=80, height=300)
+dikey_kaydirma.place(x=10, y=80, height=500)
 dikey_kaydirma["orient"] = "vertical"
 dikey_kaydirma.config(command=cikti.yview)
 cikti['yscrollcommand'] = dikey_kaydirma.set
 
 yatay_kaydirma = Scrollbar(pencere)
-yatay_kaydirma.place(x=35, y=380, width=560)
+yatay_kaydirma.place(x=35, y=590, width=700)
 yatay_kaydirma["orient"] = "horizontal"
 yatay_kaydirma.config(command=cikti.xview)
 cikti['xscrollcommand'] = yatay_kaydirma.set
 
 # Butonlar
-buton_genisligi = 8
+buton_genisligi = 10
 
 btn_donustur_y = tk.Button(pencere, 
                         text="Yatay",
                         width=buton_genisligi,
                         command=donustur_yatay)
-btn_donustur_y.place(x=10, y=415)                     
+btn_donustur_y.place(x=10, y=615)                     
 
 btn_donustur_d = tk.Button(pencere, 
                         text="Dikey",
                         width=buton_genisligi,
                         command=donustur_dikey)
-btn_donustur_d.place(x=108, y=415)  
+btn_donustur_d.place(x=130, y=615)  
 
 btn_temizle = tk.Button(pencere, 
                         text="Temizle",
                         width=buton_genisligi,
                         command=temizle)
-btn_temizle.place(x=206, y=415)  
+btn_temizle.place(x=250, y=615)  
 
 btn_kopyala = tk.Button(pencere, 
                         text="Kopyala",
                         width=buton_genisligi,
                         command=kopyala)
-btn_kopyala.place(x=304, y=415)  
+btn_kopyala.place(x=370, y=615)  
 
 btn_kaydet = tk.Button(pencere, 
                         text="Kaydet",
                         width=buton_genisligi,
                         command=kaydet)
-btn_kaydet.place(x=402, y=415)                     
+btn_kaydet.place(x=490, y=615)                     
 
 btn_kapat = tk.Button(pencere, 
                         text="Kapat",
                         width=buton_genisligi,
                         command=quit)
-btn_kapat.place(x=500, y=415)
+btn_kapat.place(x=610, y=615)
 
 
 pencere.mainloop()
